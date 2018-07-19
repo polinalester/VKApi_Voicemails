c2c535bc5b1ed92738e8371277b6496efb73361f
polinalester <epikh1gh@yandex.ru>
2018-07-20 00:37:01 +0300

    Updated files

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect('voicemails.db3')
 
 c = conn.cursor()
-c.execute('''CREATE TABLE voicemails(uid integer, vmfiles text)''')
+c.execute('''CREATE TABLE voicemails(uid integer primary key not null, vmfiles text)''')
 conn.commit()
 
 conn.close()
--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-import sys, urllib2, sqlite3
+import sys, urllib2, sqlite3, os
 uids = sys.argv[1:]
 
-with open('tokens.cfg', 'r') as f: # TODO: make cfg prettier
+with open('config/tokens.cfg', 'r') as f: # TODO: make cfg prettier
 	access_token = f.read()
 aid = 6637896 # TODO: add to cfg?
 
 from voicemails import get_voicemails
 vmails = get_voicemails(uids, aid, access_token)
 
+if not os.path.exists('voicemails'):
+    os.makedirs('voicemails')
+
 db_vmails = {}
 for uid, vmlinks in vmails.iteritems():
+	if not os.path.exists('voicemails/' + uid):
+		os.makedirs('voicemails/' + uid)
 	names = []
 	for vmlink in vmlinks:
 		mp3file = urllib2.urlopen(vmlink)
 		name = vmlink.rsplit('/', 1)[1]
 		names.append(name)
-		name = 'voicemails/' + name
+		name = 'voicemails/' + uid + '/' + name
 		with open(name,'wb') as f:
 			f.write(mp3file.read())
 	db_vmails[uid] = ', '.join(names)
@@ -25,7 +30,12 @@ c = conn.cursor()
 
 for uid, vmfiles in db_vmails.iteritems():
 	if vmfiles:
-		c.execute("INSERT INTO voicemails VALUES (" + uid + ", '" + vmfiles + "')")
+		c.execute("SELECT uid FROM voicemails WHERE uid = " + uid + " LIMIT 1")
+		row=c.fetchone()
+		if row is None:
+			c.execute("INSERT INTO voicemails VALUES (" + uid + ", '" + vmfiles + "')")
+		else:
+			c.execute("UPDATE voicemails SET vmfiles = '" + vmfiles + "' WHERE uid =" + uid)
 
 conn.commit()
 conn.close()
